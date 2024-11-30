0980a999edd681d3b92ee6be3f829dd49a8d05d3
daijunhao <121751847+daijunhaoMinecraft@users.noreply.github.com>
2024-11-30 17:01:43 +0800

    创建 Password_String_1.cs

---
+++ Hookevent/Password_String_1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using WPFLauncher.Code;
+using WPFLauncher.Network.Launcher;
+using WPFLauncher.Util;
+using System.Windows;
+using Newtonsoft.Json;
+using WPFLauncher;
+using WPFLauncher.Common;
+using WPFLauncher.Manager;
+using WPFLauncher.Manager.PCChannel;
+using WPFLauncher.Network.Message;
+using MessageBox = System.Windows.MessageBox;
+
+namespace DotNetTranstor.Hookevent
+{
+	//解决密码只能是纯数字问题(特别是联机大厅密码设置)
+	internal class Password_String_1 : IMethodHook
+	{
+		// Token: 0x0600003E RID: 62 RVA: 0x000032F0 File Offset: 0x000014F0
+		[OriginalMethod]
+		public static bool No_Password_Number(string dpu)
+		{
+			return true;
+		}
+
+		// Token: 0x0600003F RID: 63 RVA: 0x000032F4 File Offset: 0x000014F4
+		[CompilerGenerated]
+		[HookMethod("WPFLauncher.Manager.AntiIndulgence.arh", "h", "No_Password_Number")]
+		// Token: 0x06000433 RID: 1075 RVA: 0x00042D28 File Offset: 0x00040F28
+		public static string h(string nfu, string nfv = "密码")
+		{
+			return "";
+		}
+	}
+}
