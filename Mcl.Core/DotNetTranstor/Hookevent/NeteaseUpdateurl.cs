68508b64f8a2e43df67c113fd72e939310816074
daijunhao <121751847+daijunhaoMinecraft@users.noreply.github.com>
2025-11-30 11:34:12 +0800

    Fix_2

--- Mcl.Core/DotNetTranstor/Hookevent/NeteaseUpdateurl.cs
+++ Mcl.Core/DotNetTranstor/Hookevent/NeteaseUpdateurl.cs
@@ -9,11 +9,7 @@ namespace MicrosoftTranslator.DotNetTranstor.Hookevent
         [HookMethod("WPFLauncher.Manager.Configuration.axe", "c", null)]
         public string UpdateUrl()
         {
-            if (Path_Bool.NeteaseUpdateDomainhttp.EndsWith("/"))
-            {
-                return Path_Bool.NeteaseUpdateDomainhttp + "serverlist/release.json";
-            }
-            return Path_Bool.NeteaseUpdateDomainhttp + "/serverlist/release.json";
+            return Path_Bool.NeteaseUpdateDomainhttp;
         }
     }
 }
